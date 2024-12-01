0bd3a71a3379b5b2831debedd6e6020797a92679
BadonnTheDeer <badonnthedeer@protonmail.com>
2024-11-30 20:22:21 -0500

    add Poison, Force, and Psychic damage types

--- src/main/java/com/badonnthedeer/ttrpg_craft/registry/ModDamageTypes.java
+++ src/main/java/com/badonnthedeer/ttrpg_craft/registry/ModDamageTypes.java
@@ -18,6 +18,9 @@ public class ModDamageTypes {
     public static final ResourceKey<DamageType> ACID_DAMAGE =
             ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "acid_damage"));
 
+    public static final ResourceKey<DamageType> POISON_DAMAGE =
+            ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "poison_damage"));
+
     public static final ResourceKey<DamageType> LIGHTNING_DAMAGE =
             ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "lightning_damage"));
 
@@ -29,6 +32,12 @@ public class ModDamageTypes {
 
     public static final ResourceKey<DamageType> NECROTIC_DAMAGE =
             ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "necrotic_damage"));
+
+    public static final ResourceKey<DamageType> FORCE_DAMAGE =
+            ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "force_damage"));
+
+    public static final ResourceKey<DamageType> PSYCHIC_DAMAGE =
+            ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "psychic_damage"));
 
     public static final ResourceKey<DamageType> NONMAGICAL_PHYSICAL_DAMAGE =
             ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath(TTRPGCraft.MOD_ID, "nonmagical_damage"));
