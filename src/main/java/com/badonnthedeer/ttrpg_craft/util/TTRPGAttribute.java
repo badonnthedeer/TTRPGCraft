282515a747417375f7f78b3c91c5a56a756f7754
BadonnTheDeer <badonnthedeer@protonmail.com>
2025-01-05 23:06:01 -0500

    rename getTTModifier -> getModifier, add entity to input vars

--- src/main/java/com/badonnthedeer/ttrpg_craft/util/TTRPGAttribute.java
+++ src/main/java/com/badonnthedeer/ttrpg_craft/util/TTRPGAttribute.java
@@ -1,11 +1,13 @@
 package com.badonnthedeer.ttrpg_craft.util;
 
-import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 
 public class TTRPGAttribute {
 
-    public static double getTTModifier(AttributeInstance instance){
+    public static double getModifier(LivingEntity entity, AttributeInstance instance)
+    {
         return ((instance.getValue() / 2) -5) / 4;
     }
+
 }
